a45ad7231ad0e460a03437c3d374b1d279bbaaab
matthewm936 <98236645+matthewm936@users.noreply.github.com>
2023-01-29 18:27:54 -0700

    Learning Alechemy SDK for EVM requests through api

---
+++ index.js
@@ -0,0 +1,17 @@
+// Setup: npm install alchemy-sdk
+const { Network, Alchemy } = require("alchemy-sdk");
+
+// Optional Config object, but defaults to demo api-key and eth-mainnet.
+const settings = {
+  apiKey: "m0hRA99yMpl8syzdw9hQp1YUipmrXKTv", // Replace with your Alchemy API Key.
+  network: Network.ETH_MAINNET, // Replace with your network.
+};
+
+const alchemy = new Alchemy(settings);
+
+async function main() {
+  const latestBlock = await alchemy.core.getBlockNumber();
+  console.log("The latest block number is", latestBlock);
+}
+
+main();
